260e8a754dab0790110386e784ab3540b09eed9b
MANOJ SINGH RAJPUT <131298877+MANOJRAJPUT2065@users.noreply.github.com>
2023-07-16 18:18:33 +0530

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,51 @@
+// let person={
+// firstName:'Manoj',
+// semister:"1st",
+// Age:18
+// }
+// console.log(person)
+let a=5;
+let b=10;
+//console.log(a++)--->5
+// console.log(++a)-->6
+// let ans1=(++a)*(--b);
+// console.log(ans1);
+let age=18;
+// let status=(age>=18)?console.log('I can vote'):console.log('i Cannot vote ');
+// console.log(age)
+// let status=(age>=18)?'I can vote':'i Cannot vote 
+// console.log(status)
+// let marks=98;
+// if(marks>90){
+// console.log('A');
+// }
+// else if(marks>80){
+//     console.assertlog('B');
+// }
+// else if(marks>70){
+//     console.log('C');
+// }
+// else{
+//     console.log('Do Hard Work');
+// }
+
+//Switch Statement
+
+// let marks=prompt("Enter the marks");
+// switch(marks){
+// case '90':
+//     console.log('A');
+//     case '80':
+//         console.log('b');
+//         break;
+// default:
+//     console.log("Invalid Number used")
+  
+//     }
+
+// While loop
+// let x=1;
+// while(x<=5){
+//     console.log(x);
+//     x++;
+// }
